0435e9bb406082792c8b200f0004dd65c842eee3
Ishi Tran <berugen1st@gmail.com>
2023-05-05 15:44:48 +0700

    add teleport, functional and storage panel

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <script setup lang="ts">
 import Button from './components/Button.vue';
 import ButtonGroup from './components/ButtonGroup.vue';
+import FunctionalPanel from './components/FunctionalPanel.vue';
+import StoragePanel from './components/StoragePanel.vue';
 </script>
 
 <template>
-  <Button
-    label="New Teleport"
-    color="darker"
-    rounded
-  />
-  <Button
-    label="New Storage"
-    color="danger"
-    rounded
-  />
-  <Button
-    label="Scan Teleport"
-    color="neutral"
-    rounded
-  />
+  <FunctionalPanel />
 </template>
 
 <style scoped></style>
