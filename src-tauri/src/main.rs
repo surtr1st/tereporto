fedca4ed0d130b8990d406fbc51aaaf9da198c40
Ishi Tran <berugen1st@gmail.com>
2023-05-06 08:24:44 +0700

    handle system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,55 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{
+    AppHandle, CustomMenuItem, Manager, RunEvent, SystemTray, SystemTrayEvent, SystemTrayMenu,
+    SystemTrayMenuItem, WindowEvent,
+};
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .system_tray(create_system_tray())
+        .on_system_tray_event(handle_system_tray)
+        .on_window_event(|event| {
+            if let WindowEvent::CloseRequested { api, .. } = event.event() {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+        })
+        .invoke_handler(tauri::generate_handler![])
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(prevent_backend_on_close)
+}
+
+fn create_system_tray() -> SystemTray {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let open = CustomMenuItem::new("open".to_string(), "Open");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(open)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    SystemTray::new().with_menu(tray_menu)
+}
+
+fn handle_system_tray(app: &AppHandle, event: SystemTrayEvent) {
+    if let SystemTrayEvent::MenuItemClick { id, .. } = event {
+        match id.as_str() {
+            "open" => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+            }
+            "quit" => {
+                std::process::exit(0);
+            }
+            _ => {}
+        }
+    }
+}
+
+fn prevent_backend_on_close(_app: &AppHandle, event: RunEvent) {
+    if let RunEvent::ExitRequested { api, .. } = event {
+        api.prevent_exit();
+    }
 }
