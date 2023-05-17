f199ce191e462b1f260a6b8e8cf1a1821cad94fa
Ishi Tran <berugen1st@gmail.com>
2023-05-18 06:54:55 +0700

    simplify server command in client

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './styles.css';
-import { createApp } from 'vue';
 import App from './App.vue';
+import { createApp } from 'vue';
 import { setupI18n } from './i18n';
 
 const i18n = setupI18n();
